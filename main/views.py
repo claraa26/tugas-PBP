b81b3a51bc137e4976dcb84457af104629cc71d0
claraa26 <clarasistaa26@gmail.com>
2023-09-13 03:35:33 +0700

    bisa deploy

--- main/views.py
+++ main/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'name': 'Clara',
-        'class': 'PBP E'
+        'class': 'math',
+        'slot available': '15',
+        'description': 'math class available for 15 person'
+
     }
 
     return render(request, "main.html", context)
